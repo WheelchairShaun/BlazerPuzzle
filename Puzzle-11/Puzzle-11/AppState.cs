2ce50412d3652d108ad7eaeb40d1bd37838cc67f
Shaun Barnes <shaun.w.barnes@gmail.com>
2023-11-12 16:19:00 -0500

    Add Puzzle 11

---
+++ Puzzle-11/Puzzle-11/AppState.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+public class AppState : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private string message = string.Empty;
+    public string Message
+    {
+        get => message;
+        set
+        {
+            if (message != value)
+            {
+                message = value;
+                OnPropertyChanged(nameof(Message));
+            }   
+        }
+    }
+}
